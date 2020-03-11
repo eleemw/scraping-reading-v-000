5addc1e65da8c55d314cce9de4c291ddd6484ddd
Erica Murdock-Waters <ericahpa@gmail.com>
2020-03-11 19:41:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,4 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
-Nokogiri::HTML(html) #take the string of HTML returned by open-uri's open method and convert it into a NodeSet and save it to variable `doc`
+doc = Nokogiri::HTML(html) #take the string of HTML returned by open-uri's open method and convert it into a NodeSet and save it to variable `doc`
